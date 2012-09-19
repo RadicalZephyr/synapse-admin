3ec4154d6083f5ab9ba9a4a6ef699390ff4620fa
Geoff Shannon <earthlingzephyr@gmail.com>
2012-09-19 09:18:52 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to synapse-admin
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/synapse_admin/core_test.clj
@@ -0,0 +1,7 @@
+(ns synapse-admin.core-test
+  (:use clojure.test
+        synapse-admin.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
