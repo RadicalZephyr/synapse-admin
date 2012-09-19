435513fdb029d32c9051f69dce481dcac8f5a5df
Geoff Shannon <earthlingzephyr@gmail.com>
2012-09-19 09:39:28 -0700

    Add dependencies and imports

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject synapse-admin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An administration client for Synapse"
+  :url "http://github.com/ezephyr/synapse-admin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [seesaw "1.4.2"]
+                 [org.sagebionetworks/synapseJavaClient "1.8.0"]]
+  :repositories [["sagebionetworks-releases-local"
+                  "http://sagebionetworks.artifactoryonline.com/sagebionetworks/libs-releases-local"]])
--- src/synapse_admin/core.clj
+++ src/synapse_admin/core.clj
@@ -1,4 +1,6 @@
-(ns synapse-admin.core)
+(ns synapse-admin.core
+  (:use seesaw.core)
+  (:import org.sagebionetworks.client.Synapse))
 
 (defn -main
   "I don't do a whole lot."
