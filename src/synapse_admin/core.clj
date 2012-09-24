7c83bb50b9505cb7a6fa43d354063150cdb98060
Geoff Shannon <earthlingzephyr@gmail.com>
2012-09-24 14:57:29 -0700

    Add input validation

--- src/synapse_admin/core.clj
+++ src/synapse_admin/core.clj
@@ -2,17 +2,32 @@
   (:use seesaw.core)
   (:import org.sagebionetworks.client.Synapse))
 
+(defn display-main-page
+  "Display the main admin page"
+  [root]
+  )
+
+(defn validate-email [email]
+  (re-matches #"(?i)^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" email))
+
 (defn login [event]
+  "Fire a login event"
   (let [{:keys [username password]} (group-by-id (to-root event))
+        username (value username)
+        password (value password)
         synapse (Synapse.)]
-    (try
-      (.login synapse
-            (config username :text)
-            (config password :text))
-      (alert "You've successfully logged in to Synapse!")
-      (catch
-          org.sagebionetworks.client.exceptions.SynapseBadRequestException ex
-        (alert ex)))))
+    (cond (and (not (= "" password))
+               (validate-email username))
+          (try
+            (.login synapse
+                    username
+                    password)
+                    (alert "You've successfully logged in to Synapse!")
+                    (catch
+                        org.sagebionetworks.client.exceptions.SynapseBadRequestException ex
+                      (alert "Unable to login, make sure your username/password are correct.")))
+          (= "" password) (alert "Your password cannot be blank.")
+          :else (alert "Please enter a valid email address"))))
 
 (defn login-widget
   "Create the login widget"
